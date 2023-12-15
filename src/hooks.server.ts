459ccbb9dc96c77b9b770c19c0dd9958e4268ece
Rony Philip <ronyphilip751@gmail.com>
2023-12-15 18:12:55 +0000

    spell fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
 export const handle = async ({ event, resolve }) => {
-  event.locals.pb = new PocketBase("https://https://rony-blog.pockethost.io");
+  event.locals.pb = new PocketBase("https://rony-blog.pockethost.io");
   const response = await resolve(event);
   
   return response;
